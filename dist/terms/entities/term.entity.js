8b11c6728f3fa10c44e39d1ffc3a9feae4faf5d8
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-04-23 18:51:42 +0100

    feat(terms): add currentWeek and totalWeeks fields to Term entity
    - Introduced currentWeek and totalWeeks fields in the Term GraphQL schema.
    - Implemented logic to calculate totalWeeks based on start and end dates.
    - Added currentWeek calculation to determine the current week based on today's date.

--- dist/terms/entities/term.entity.js
+++ dist/terms/entities/term.entity.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var Term_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Term = void 0;
 const graphql_1 = require("@nestjs/graphql");
@@ -16,6 +17,8 @@ const base_entity_1 = require("../../common/entities/base.entity");
 const enums_1 = require("../../common/enums");
 const session_entity_1 = require("./session.entity");
 let Term = class Term extends base_entity_1.BaseEntity {
+    static { Term_1 = this; }
+    static DAY_IN_MS = 24 * 60 * 60 * 1000;
     name;
     sessionId;
     schoolId;
@@ -23,6 +26,32 @@ let Term = class Term extends base_entity_1.BaseEntity {
     endDate;
     status;
     totalSchoolDays;
+    normalizeToUtcDateOnly(value) {
+        const date = new Date(value);
+        return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()));
+    }
+    get totalWeeks() {
+        const start = this.normalizeToUtcDateOnly(this.startDate);
+        const end = this.normalizeToUtcDateOnly(this.endDate);
+        if (end < start)
+            return 0;
+        const daysInclusive = Math.floor((end.getTime() - start.getTime()) / Term_1.DAY_IN_MS) + 1;
+        return Math.ceil(daysInclusive / 7);
+    }
+    get currentWeek() {
+        const start = this.normalizeToUtcDateOnly(this.startDate);
+        const end = this.normalizeToUtcDateOnly(this.endDate);
+        if (end < start)
+            return 0;
+        const today = new Date();
+        const todayUtc = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate()));
+        if (todayUtc < start)
+            return 0;
+        if (todayUtc > end)
+            return this.totalWeeks;
+        const elapsedDays = Math.floor((todayUtc.getTime() - start.getTime()) / Term_1.DAY_IN_MS) + 1;
+        return Math.min(this.totalWeeks, Math.ceil(elapsedDays / 7));
+    }
     session;
 };
 exports.Term = Term;
@@ -62,12 +91,22 @@ __decorate([
     __metadata("design:type", Number)
 ], Term.prototype, "totalSchoolDays", void 0);
 __decorate([
+    (0, graphql_1.Field)(() => graphql_1.Int),
+    __metadata("design:type", Number),
+    __metadata("design:paramtypes", [])
+], Term.prototype, "totalWeeks", null);
+__decorate([
+    (0, graphql_1.Field)(() => graphql_1.Int),
+    __metadata("design:type", Number),
+    __metadata("design:paramtypes", [])
+], Term.prototype, "currentWeek", null);
+__decorate([
     (0, graphql_1.Field)(() => session_entity_1.Session),
     (0, typeorm_1.ManyToOne)(() => session_entity_1.Session, (session) => session.terms),
     (0, typeorm_1.JoinColumn)({ name: 'sessionId' }),
     __metadata("design:type", session_entity_1.Session)
 ], Term.prototype, "session", void 0);
-exports.Term = Term = __decorate([
+exports.Term = Term = Term_1 = __decorate([
     (0, graphql_1.ObjectType)(),
     (0, typeorm_1.Entity)('terms')
 ], Term);
